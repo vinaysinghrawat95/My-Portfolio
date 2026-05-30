ea895fe3c95383ef008945a74b6658f11e027a6a
Vinay Singh Rawat <vinaysinghrawat505@gmail.com>
2026-05-30 13:39:10 +0530

    implementing java script file

--- script.js
+++ script.js
@@ -1,7 +1,48 @@
 const toggleBtn = document.querySelector("#toggleBtn");
 const infoMore = document.querySelector(".info-more");
+const navLinks = document.querySelectorAll(".navbar-link");
+const hamburgerLinks = document.querySelectorAll(".hamburger-navbar-link");
+const articles = document.querySelectorAll("article");
+
+
 
 toggleBtn.addEventListener("click", ()=>{
     infoMore.classList.toggle("visible");
     toggleBtn.textContent = infoMore.classList.contains("visible") ? '▲' : '▼';
 });
+
+function switchTabs(targetSection){
+
+    articles.forEach(function(article){
+        article.classList.remove("active");
+    });
+
+    navLinks.forEach(function(navLink){
+        navLink.classList.remove("active");
+    });
+
+    hamburgerLinks.forEach(function(hamburgerLink){
+        hamburgerLink.classList.remove("active");
+    });
+
+    document.querySelector("." + targetSection).classList.add("active");
+
+    document.querySelectorAll("[data-section=" + targetSection + "]").forEach(function(btn){
+        btn.classList.add("active");
+    });
+
+}
+
+navLinks.forEach((navLink)=>{
+    navLink.addEventListener("click", ()=>{
+        switchTabs(navLink.dataset.section);
+    });
+});
+
+hamburgerLinks.forEach((hamburgerLink)=>{
+    hamburgerLink.addEventListener("click", ()=>{
+        switchTabs(hamburgerLink.dataset.section);
+    });
+});
+
+switchTabs("about");
